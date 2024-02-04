40b6f5d68450dbbf4384ca9ad71c955c21ddb576
9812598 <9812598@gmail.com>
2024-02-04 15:53:48 +0300

    add main data

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
 ﻿import React from "react";
 import { createRoot } from "react-dom/client";
+import "normalize.css";
 import "./index.css";
 
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
 root.render(
   <>
-    <h1>Hello React!</h1>
-    <p>This is a new text</p>
+    <div className="container">
+      <header className="header">
+        <div className="menuWrapper">
+          <div className="title">
+            <div className="titleName">CAT</div>
+            <div className="titleDescription">currencies academic terms</div>
+          </div>
+          <div className="currencySelect">
+            <select name="currency" id="currency"></select>
+          </div>
+        </div>
+        <div className="cat">
+          <img src="kitten.png" alt="Kitten" />
+        </div>
+      </header>
+      <main className="main">Russian Ruble</main>
+    </div>
   </>
 );
